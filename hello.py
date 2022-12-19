2421f9cfbbb7eab657ee2fbbbf8a5d2fa1afa93b
Derek Warmington <derekwarming@gmail.com>
2022-12-19 14:44:58 -0800

    added date and time to user profile

--- hello.py
+++ hello.py
@@ -5,17 +5,29 @@ from flask import redirect
 from flask import abort
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+#shortcut icon located in bootstrap/base.html
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",
+    current_time=datetime.utcnow())
 
 @app.route("/user/<string:name>")
 def user(name):
-    return render_template("user.html", name=name)
+    return render_template("user.html", name=name,
+    current_time=datetime.utcnow())
+
+@app.route("/datetime-now")
+def datetimenow():
+    return render_template("datetime-now.html",
+    current_time=datetime.utcnow())
 
 @app.errorhandler(404) #404 error page
 def page_not_found(e):
